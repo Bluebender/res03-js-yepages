5d27d593b9cdc81b75bd8aa6fb36b7697d1c6740
Bluebender <olliviev@gmail.com>
2023-01-24 11:16:55 +0000

    Sauvegarde de 12h15

--- yepages/assets/js/classes/UserManager.js
+++ yepages/assets/js/classes/UserManager.js
@@ -3,7 +3,7 @@ import { User } from './User.js';
 class UserManager {
     #users;
 
-    constructor(users) {
+    constructor(users = []) {
         this.#users = users;
     }
     get users() {
@@ -39,7 +39,36 @@ class UserManager {
         }
     }
     createUser(user){
-        this.#users.push(user);
+
+        let userId = this.#users.length+1;
+        let userUsername = document.getElementById("userName").value;
+        let userEmail = document.getElementById("email").value;
+        let userPassword = document.getElementById("password").value;
+        let userSecondPassword = document.getElementById("secondPassword").value;
+        let userFirstName = document.getElementById("firstName").value;
+        let userLastName = document.getElementById("lastName").value;
+        let userProfileImage = document.getElementById("url").value;
+        
+
+        let doublonEmail=false
+        for (let i=0; i<this.#users.length; i++){
+            if (this.#users[i].email===userEmail){
+                doublonEmail=true;    
+            }
+        }
+
+        if (userPassword!==userSecondPassword){
+            alert("Les deux mots de passe ne sont pas identiques");
+        }
+        else if (doublonEmail===true){
+            alert("L'adresse email existe déjà");
+        }
+        else{
+            
+            let user = new User(userId, userUsername, userEmail, userPassword, userFirstName, userLastName, userProfileImage)
+    
+            this.#users.push(user);
+        }
     }
     deleteUser(userId){
         let usersListWithoutId = [];
@@ -65,14 +94,30 @@ class UserManager {
         let usersBackup = JSON.parse(sessionStorage.getItem("usersBackup"));
 
         // Je parse chaque élément du tableau
-        for (let i=0; i<usersBackup.length; i++){
-            let userBackup = JSON.parse(usersBackup[i]);
-            let user = new User(userBackup.id, userBackup.username, userBackup.email, userBackup.password, userBackup.firstName, userBackup.lastName, userBackup.profileImage);
-            this.#users.push(user);
+        if (usersBackup===null){
+            this.#users=[];
+        }
+        else{
+            for (let i=0; i<usersBackup.length; i++){
+                let userBackup = JSON.parse(usersBackup[i]);
+                let user = new User(userBackup.id, userBackup.username, userBackup.email, userBackup.password, userBackup.firstName, userBackup.lastName, userBackup.profileImage);
+                this.#users.push(user);
+            }
         }
     }
     login(email, password){
-        
+        let userEmail = document.getElementById("connexionEmail").value;
+        let userPassword = document.getElementById("connexionPassword").value;
+        for (let i=0; i<this.#users.length; i++){
+            if (this.#users[i].email===userEmail && this.#users[i].password===userPassword){
+                alert("Vous êtes bien connecté");
+                break;
+            }
+            else{
+                alert("Votre connexion a échouée");
+                break;
+            }
+        }
     }
 }
 export { UserManager };
--- yepages/assets/js/index.js
+++ yepages/assets/js/index.js
@@ -6,85 +6,21 @@ import { UserManager } from './classes/UserManager.js';
 
 window.addEventListener("DOMContentLoaded", function(){
 
-    // let book1 = new Book(0, "La reine des neiges", "Disney", 2012, 124, "Il était une fois...", "assets/img/image1.png");
-    // let book2 = new Book(1, "Le roi lion", "Disney", 2010, 138, "Il était une fois...", "assets/img/image2.png");
-    // let book3 = new Book(2, "La petite sirène", "Disney", 2008, 149, "Il était une fois...", "assets/img/image3.png");
-
-    // let user1 = new User(0, "Vincento", "vincent@gmail.com", 1234, "Vincent", "Ollivier", "assets/img/image4.png");
-    // let user2 = new User(1, "Natha", "nathanael@gmail.com", 2345, "Nathanaël", "Ollivier", "assets/img/image5.png");
-    // let user3 = new User(2, "Clem", "clementine@gmail.com", 3456, "Clémentine", "Ollivier", "assets/img/image6.png");
-    
-    // let books = [book1, book2, book3];
-    // let users = [user1, user2, user3];
-    
-    // console.log(books)
-    
-    // let newManage = new BookManager(books)
-    // console.log(newManage)
-    // newManage.save(); 
-    // console.log(newManage)
-    // newManage.load(); 
-    // console.log(newManage.findAllBooks());
-    // console.log(newManage.findBookById(2))
-    // console.log(newManage.findBooksByTitle("Le roi lion"));
-    // console.log(newManage.findBooksByAuthor("Disney"));
-    // console.log(newManage.findBooksByPublicationYear(2012));
-    // let newBook = new Book(0, "Ratatouille", "Disney", 2011, 134, "Il était une fois...", "assets/img/image9.png");
-    // newManage.createBook(newBook);
-    // console.log(newManage)
-
-    // newManage.deleteBook(1);
-    // console.log(newManage)
-    
-    // let newBook = new Book(1, "Ratatouille", "Disney", 2011, 134, "Il était une fois...", "assets/img/image9.png");
-    // newManage.editBook(newBook)    
-    // console.log(newManage)
-    let users = [];
-    let newUserManage = new UserManager(users);
+    let newUserManage = new UserManager();
+    newUserManage.load();
 
     let creationForm = document.getElementById("creationForm");
     creationForm.addEventListener("submit", function(event){
         event.preventDefault();
-        
-        
-        
-        
-        let userId =users.length+1;
-        let userUsername = document.getElementById("userName").value;
-        let userEmail = document.getElementById("email").value;
-        let userPassword = document.getElementById("password").value;
-        let userSecondPassword = document.getElementById("secondPassword").value;
-        let userFirstName = document.getElementById("firstName").value;
-        let userLastName = document.getElementById("lastName").value;
-        let userProfileImage = document.getElementById("url").value;
-        
+        newUserManage.createUser();
         newUserManage.save();
-        newUserManage.load();
-        let doublonEmail=false
-        for (let i=0; i<newUserManage.users.length; i++){
-            if (newUserManage.users[i].email===userEmail){
-                doublonEmail=true;    
-            }
-        }
 
-        if (userPassword!==userSecondPassword){
-            alert("Les deux mots de passe ne sont pas identiques");
-        }
-        else if (doublonEmail===true){
-            alert("L'adresse email existe déjà");
-        }
-        else{
-            
-        let user = new User(userId, userUsername, userEmail, userPassword, userFirstName, userLastName, userProfileImage)
-
-        newUserManage.createUser(user);
-        newUserManage.load();
-        newUserManage.save();
-        }
-
-        
     });
     
-        
-    
+    let connectionButton = document.getElementById("connectionButton");
+    connectionButton.addEventListener("submit", function(event){
+        event.preventDefault();
+        newUserManage.login();
+
+    });
 });
